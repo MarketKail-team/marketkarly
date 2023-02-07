65e0eceed1073541f7ecef6d47db9adb38fec3cc
chaewon <hjh7814@naver.com>
2023-02-07 09:26:06 +0900

    Fix home JS  (#81)

--- client/js/include/swiper-main.js
+++ client/js/include/swiper-main.js
@@ -4,50 +4,66 @@ const mainSwiper = new Swiper('.swiper-recommend', {
   slidesPerGroup: 4,
   loop: true,
   loopFillGroupWithBlank: true,
-  // pagination: {
-  //   el: '.swiper-pagination',
-  //   clickable: true
-  // },
-  observer: true,
-  observeParents: true,
+  // observer: true,
+  // observeParents: true,
   navigation: {
     nextEl: '.swiper-recommend-button-next',
     prevEl: '.swiper-recommend-button-prev'
+  },
+  keyboard: {
+    enabled: true,
+    onlyInViewport: false,
+  },
+  a11y: {
+    containerRoleDescriptionMessage: '추천 상품',
+    nextSlideMessage: '다음 슬라이드 버튼 입니다',
+    prevSlideMessage: '이전 슬라이드 버튼 입니다',
   }
 });
+mainSwiper.on('reachBeginning', function () {
+  console.log('hi')
+});
 
-mainSwiper = new Swiper('.swiper-best-price', {
+const mainSwiper2 = new Swiper('.swiper-best-price', {
   slidesPerView: 4,
   spaceBetween: 18,
   slidesPerGroup: 4,
   loop: true,
   loopFillGroupWithBlank: true,
-  // pagination: {
-  //   el: '.swiper-pagination',
-  //   clickable: true
-  // },
   navigation: {
     nextEl: '.swiper-best-price-button-next',
     prevEl: '.swiper-best-price-button-prev'
   },
-  observer: true,
-  observeParents: true,
+  a11y: {
+    containerRoleDescriptionMessage: '가격 할인 상품',
+    nextSlideMessage: '다음 슬라이드 버튼 입니다',
+    prevSlideMessage: '이전 슬라이드 버튼 입니다',
+  }
+
+
 });
 
-// 최근본상품 버튼 동작 안됨 - 수정필요
-const swiperAside = new Swiper('.swiper-aside', {
+
+const asideSwiper = new Swiper('.aside-swiper', {
+  direction: 'vertical',
   slidesPerView: 3,
+  grid: {
+    column: 2,
+  },
+
   spaceBetween: 10,
   slidesPerGroup: 3,
   loop: true,
   loopFillGroupWithBlank: true,
-  pagination: {
-    el: '.aside-swiper-pagination',
-    clickable: true,
-  },
   navigation: {
     nextEl: '.aside-swiper-button-next',
     prevEl: '.aside-swiper-button-prev',
-  }, observer: true,
-  observeParents: true,
+  },
+  a11y: {
+    containerRoleDescriptionMessage: '최근 본 상품',
+    nextSlideMessage: '다음 슬라이드 버튼 입니다',
+    prevSlideMessage: '이전 슬라이드 버튼 입니다',
+  }
 });
+
+
