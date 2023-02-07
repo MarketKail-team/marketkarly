d03d735bffa0da92a4879df4d5ff69f5ed84cf5d
chaewon <hjh7814@naver.com>
2023-02-07 20:20:07 +0900

    Fix Error correction in home(#107)

--- client/js/include/product_list.js
+++ client/js/include/product_list.js
@@ -1,10 +1,8 @@
-import { getNode, addClass, removeClass } from '../../lib/index.js';
+import { getNode } from '../../lib/index.js';
 
 const hiddenInfo = getNode('.hidden_info');
 const questinMark = getNode('.question__mark');
 
-
-//유틸함수 왜 안되는지..?
 
 questinMark.addEventListener('mouseover', () => {
   hiddenInfo.classList.add('is-active');
